f61f84b484e8eb9096ca253f184af229bf99f89f
Martin <martin.drugge@telia.com>
2020-08-31 13:56:28 +0200

    Test

---
+++ Main.py
@@ -0,0 +1,9 @@
+def main():
+
+    print("Mitt namn är Martin Drugge och är uppvuxen i Arvika, Värmland.")
+    print("Jag har inte programmerat på riktigt tidigare men har fått vissa grundkoncept förklarade för mig.")
+    print("Jag valde KYH eftersom jag tyckte utbildningen verkade väldigt lockande.")
+
+
+if __name__ == '__main__':
+    main()
