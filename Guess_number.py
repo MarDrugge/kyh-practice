9beff5b5d3c2d689190c33db480a9b035b8017a0
Martin <martin.drugge@telia.com>
2020-09-01 11:09:18 +0200

    -

--- Guess_number.py
+++ Guess_number.py
@@ -4,18 +4,25 @@ n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1 & 100. Vilket?")
 chances = 0
 
-while True:
-    text = input("Din Gissning: ")
-    as_number = int(text)
 
-    chances += 1
-    if as_number == n:
-        print("Korrekt!")
-        print("Antal gissningar:", chances)
-        break
+def mainloop():
+    global chances
+    while True:
+        text = input("Din Gissning: ")
+        as_number = int(text)
 
-    if as_number < n:
-        print("Fel, mitt nummer är högre. Försök igen!")
+        chances += 1
+        if as_number == n:
+            print("Korrekt!")
+            print("Antal gissningar:", chances)
+            break
 
-    if as_number > n:
-        print("Fel, mitt nummer är lägre. Försök igen!!")
+        if as_number < n:
+            print("Fel, mitt nummer är högre. Försök igen!")
+
+        if as_number > n:
+            print("Fel, mitt nummer är lägre. Försök igen!!")
+    return
+
+
+mainloop()
