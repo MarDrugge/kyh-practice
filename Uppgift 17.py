a1e4cf7fc9838e967eb7e955c490f3b908ca0597
Martin <martin.drugge@telia.com>
2020-09-09 11:38:56 +0200

    Alt 1 klar

--- Uppgift 17.py
+++ Uppgift 17.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 
 
+def alt_1():
+    for line in Path('TODO.txt').read_text(encoding='utf8').splitlines():
+        print(line)
+
+
 def main():
     alt = input(f'1. Lista TODO:\n2. Lägg till uppgift:\n3. Ta bort uddgift:\n4. Avbryt programmet:\nVälj alternativ: ')
+    if alt == "1":
+        alt_1()
     if alt == "4":
         print("Programmet avbryts")
 
