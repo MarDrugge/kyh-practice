55e4e40b60243bc4a302977073e52c5ca7bb1e57
Martin <martin.drugge@telia.com>
2020-09-10 10:55:05 +0200

    changes

--- Uppgift 17.py
+++ Uppgift 17.py
@@ -1,15 +1,28 @@
 from pathlib import Path
 
 
+lines = Path('TODO.txt')
+read = lines.read_text(encoding='utf8')
+split = read.splitlines()
+n = "\n"
+
+
 def alt_1():
-    for line in Path('TODO.txt').read_text(encoding='utf8').splitlines():
-        print(line)
+    print(n.join(split))
+
+
+def alt_2():
+    ny = input("Lägg till Uppgift: ").strip()
+    split.append(ny)
+    lines.write_text(f'{n.join(split)}', encoding='utf8')
 
 
 def main():
-    alt = input(f'1. Lista TODO:\n2. Lägg till uppgift:\n3. Ta bort uddgift:\n4. Avbryt programmet:\nVälj alternativ: ')
+    alt = input(f'1. Lista TODO:\n2. Lägg till uppgift:\n3. Ta bort uppgift:\n4. Avbryt programmet:\nVälj alternativ: ')
     if alt == "1":
         alt_1()
+    if alt == "2":
+        alt_2()
     if alt == "4":
         print("Programmet avbryts")
 
