e8a4ed504807b5322f0495fdafd1bd0970dcfe72
Martin <martin.drugge@telia.com>
2020-09-16 10:35:12 +0200

    -

--- uppgift24.py
+++ uppgift24.py
@@ -1,14 +1,22 @@
 import requests
+import datetime
 from pprint import pprint
 
 
 def main():
     r = requests.get("https://proagile.se/api/publicEvents")
     t = r.json()
-    #pprint(t)
-    for course in t:
-        if 'startDate' in course:
-            print(course['startDate'])
+    today = datetime.date.today()
+
+    year = input("Skriv år: ")
+    month = input("Skriv månad: ")
+    date1 = f'{year}-{month}-01'
+    date2 = f'{year}-{month}-31'
+
+    for elements in t:
+       if elements['startDate'] >= date1 and elements['startDate'] <= date2:
+           print(f'Kursnamn: {elements["name"]}\nStartdatum: {elements["startDate"]}\nSlutdatum: {elements["endDate"]}')
+           print(f'\n\n')
 
 
 if __name__ == '__main__':
