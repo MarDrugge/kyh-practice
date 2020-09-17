eab6c442b5c3f9f4a08d53263ae4ac573d522898
Martin <martin.drugge@telia.com>
2020-09-17 13:23:18 +0200

    fungerande test

--- test_conversions.py
+++ test_conversions.py
@@ -1,13 +1,13 @@
 from conversions import m_to_mm, cm_to_m
 
 
-def test_m_to_mm(self):
+def test_m_to_mm():
     expected = 1000
     got = m_to_mm(m=1)
     assert expected == got
 
 
-def test_cm_to_m(self):
+def test_cm_to_m():
     expected = 2
     got = cm_to_m(cm=200)
     assert expected == got
