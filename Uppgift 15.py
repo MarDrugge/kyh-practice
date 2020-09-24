f0a6bf5559fd75da1cf3cb78d09e6e64d498cc4e
Martin <martin.drugge@telia.com>
2020-09-24 13:13:38 +0200

    Klar

--- Uppgift 15.py
+++ Uppgift 15.py
@@ -2,6 +2,7 @@
 def main():
     vowels = "aeiouy"
     vowel_count = 0
+
     def wordcount(txt):
         return len(txt.split())
     text = input("Skriv en text: ")
@@ -10,6 +11,11 @@ def main():
         if c in vowels:
             vowel_count += 1
     print(f'Antal ord: {count} \nAntal vokaler: {vowel_count}')
+
+    runners_in_order = 'Lisa Lasse Louise Leopold Lova Love Lennart Lena Lisette Linus'.split()
+    vem = input('Ange löpare du söker placering för: ')
+    index = runners_in_order.index(vem)
+    print(f'Löparen hamnade på plats nr {index}')
 
 
 main()
