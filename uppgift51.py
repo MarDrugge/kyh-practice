6452d7b082e35e512874d774aba7ce202d0ed099
Martin <martin.drugge@telia.com>
2020-10-27 11:21:04 +0100

    klar

---
+++ uppgift51.py
@@ -0,0 +1,81 @@
+# 51.1
+
+# def add_as_def(a, b):
+#     return a + b
+#
+# add_as_lambda = lambda a, b: a + b
+# # print(add_as_def(3, 4))
+# # print(add_as_lambda(3, 4))
+
+# 51.2
+
+# obj = lambda s: s.upper()
+#
+# def obj(s):
+#     return s.upper()
+
+# 51.3
+
+# join_as_lambda = lambda strings, inbetween: inbetween.join(strings)
+#
+# # print(join_as_lambda("adasd", ","))
+#
+# def inbetween_fnc(strings, inbetween):
+#     return inbetween.join(strings)
+#
+# # print(inbetween_fnc("asdasdasf", ","))
+
+# 51.4
+
+# anna = ("Anna", "Persson", 42)
+# lova = ("Lova", "Andersson", 35)
+# alex = ("Alex", "Börjesson", 10)
+# people = [anna, lova, alex]
+# on_surname = sorted(people, key=lambda p: p[1])
+# for (first, last, age) in on_surname:
+#     print(f"{first} {last} ({age} år)")
+
+# 51.5
+
+# anna = ("Anna", "Persson", 42)
+# lova = ("Lova", "Andersson", 35)
+# alex = ("Alex", "Börjesson", 10)
+# people = [anna, lova, alex]
+# on_surname = sorted(people, key=lambda p: p[2])
+# for (first, last, age) in on_surname:
+#     print(f"{first} {last} ({age} år)")
+
+# 51.6
+
+
+# anna = ("Anna", "Persson", 42)
+# lova = ("Lova", "Andersson", 35)
+# alex = ("Alex", "Börjesson", 10)
+# people = [anna, lova, alex]
+# def age_sorter(people):
+#     return people[2]
+# people.sort(key=age_sorter)
+# for (first, last, age) in people:
+#     print(f"{first} {last} ({age} år)")
+
+# 51.7
+
+# from operator import attrgetter
+# class People:
+#     def __init__(self, first, last, age):
+#         self.first = first
+#         self.last = last
+#         self.age = age
+#
+# anna = People("Anna", "Persson", 42)
+# lova = People("Lova", "Andersson", 35)
+# alex = People("Alex", "Börjesson", 10)
+#
+# people_ls = [anna, lova, alex]
+#
+# people_ls.sort(key=attrgetter('age'))
+# for elem in people_ls:
+#     print(f"{elem.first} {elem.last} ({elem.age} år)")
+
+# 51.8
+
