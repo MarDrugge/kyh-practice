aa28c219cd6042238d012b74f73f9b1b47843f2a
Martin <martin.drugge@telia.com>
2020-11-12 12:59:48 +0100

    Klar

--- Uppgift 17.py
+++ Uppgift 17.py
@@ -8,7 +8,10 @@ n = "\n"
 
 
 def alt_1():
-    print(n.join(split))
+    num = 0
+    for elem in split:
+        num += 1
+        print(f'{num}. {elem}')
 
 
 def alt_2():
@@ -17,14 +20,25 @@ def alt_2():
     lines.write_text(f'{n.join(split)}', encoding='utf8')
 
 
+def alt_3():
+    del_task = int(input("Radera uppgift: "))
+    index = del_task - 1
+    del split[index]
+    lines.write_text(f'{n.join(split)}', encoding='utf8')
+
+
 def main():
-    alt = input(f'1. Lista TODO:\n2. Lägg till uppgift:\n3. Ta bort uppgift:\n4. Avbryt programmet:\nVälj alternativ: ')
-    if alt == "1":
-        alt_1()
-    if alt == "2":
-        alt_2()
-    if alt == "4":
-        print("Programmet avbryts")
+    while True:
+        alt = input(f'1. Lista TODO:\n2. Lägg till uppgift:\n3. Ta bort uppgift:\n4. Avbryt programmet:\nVälj alternativ: ')
+        if alt == "1":
+            alt_1()
+        elif alt == "2":
+            alt_2()
+        elif alt == "3":
+            alt_3()
+        elif alt == "4":
+            print("Programmet avbryts")
+            break
 
 
 if __name__ == '__main__':
